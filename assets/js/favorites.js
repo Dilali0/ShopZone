9c1ea57d404ce80a7af4927dca85c54ab6aabf04
tougha sara <toughasarah@gmail.com>
2024-11-07 16:48:46 +0100

    partie 1 terminer

---
+++ assets/js/favorites.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const ajoutfavorie = document.querySelectorAll("button[onclick^='add']");
+    const supprfavorie = document.querySelectorAll("button[onclick^='delet']");
+});
+
+function ajouteraufavories() {
+    g
+}
+
+function supprimerdefavories() {
+    g
+}
