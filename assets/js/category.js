514101c72d9eee99ae53a46966edda571d2be182
hamza2024web <hamzaakroubi1528@gmail.com>
2024-11-10 17:06:09 +0100

    the first version with map in json

--- assets/js/category.js
+++ assets/js/category.js
@@ -5,16 +5,16 @@ fetch('../../Data.json')
 
 function displaycategories(prods){
 const row = document.getElementById("row");
-    prods.forEach(cat => {
+    prods.map(cat => {
 row.innerHTML += `    
     <div class="col-md-4 mb-4">
         <div class="card category-card">
             <img  src="${cat.image}" class="card-img-top" alt="Électronique">
                 <div class="card-body text-center">
                     <h5 class="card-title">${cat.name}</h5>
-                    <p>${cat.price}</p>
+                    <p>$${cat.price}</p>
                     <p class="card-text"></p>
-                    <a href="#" class="btn btn-primary"></a>
+                    <a href="#" class="btn btn-primary">Details</a>
                 </div>
         </div>
      </div>
